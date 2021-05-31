4922c9146b09777d06a1702111ba599836769914
Meghan Bock <meghan.bock@gmail.com>
2021-05-30 21:43:41 -0500

    changes to generate random password logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// Assignment code here
-
-// Get references to the #generate element
+// initialize global variables
 var generateBtn = document.querySelector("#generate");
 var passwordObj = [
   {
@@ -12,16 +10,9 @@ var passwordObj = [
   },
 ];
 
-// select password criteria
+// function for selecting password criteria
 var selectCriteria = function () {
-
-  /*var char = 0;
-  var lower = false;
-  var upper = false;
-  var numeric = false;
-  var special = false;*/
-
-  // first prompt: # of characters (min 8, min 128)
+  // first prompt: # of characters
   passwordObj.char = window.prompt(
     "How many characters would you like your password to be? Select a number between 8 and 128."
   );
@@ -29,29 +20,25 @@ var selectCriteria = function () {
 
   // validation: min 8, min 128
   if (passwordObj.char >= 8 && passwordObj.char <= 128) {
-    // second prompt: include lowercase? (OK,CANCEL)
+    // second prompt: include lowercase?
     passwordObj.lower = window.confirm(
-      "Would you like your password to contain lowercase letters? Select OK for yes or CANCEL for no."
+      "Would you like your password to contain lowercase letters? \nSelect OK for yes or CANCEL for no."
     );
-    console.log("lower: " + passwordObj.lower);
 
-    // third prompt: include uppercase? (yes,no)
+    // third prompt: include uppercase?
     passwordObj.upper = window.confirm(
-      "Would you like your password to contain uppercase letters? Select OK for yes or CANCEL for no."
+      "Would you like your password to contain uppercase letters? \nSelect OK for yes or CANCEL for no."
     );
-    console.log("upper: " + passwordObj.upper);
 
-    // fourth prompt: include numeric? (yes,no)
+    // fourth prompt: include numeric?
     passwordObj.numeric = window.confirm(
-      "Would you like your password to contain numbers? Select OK for yes or CANCEL for no."
+      "Would you like your password to contain numbers? \nSelect OK for yes or CANCEL for no."
     );
-    console.log("numeric: " + passwordObj.numeric);
 
-    // fifth prompt: include special characters? (yes,no)
+    // fifth prompt: include special characters?
     passwordObj.special = window.confirm(
-      "Would you like your password to contain special characters? Select OK for yes or CANCEL for no."
+      "Would you like your password to contain special characters? \nSelect OK for yes or CANCEL for no."
     );
-    console.log("special: " + passwordObj.special);
 
     // final validation: make sure at least one criteria selected
     if (
@@ -60,27 +47,41 @@ var selectCriteria = function () {
       passwordObj.numeric == false &&
       passwordObj.special == false
     ) {
+      // alert user that criteria is invalid & re-start prompts
       window.alert(
-        "You did not select sufficient criteria for your password. Please try again."
+        "You did not select sufficient criteria to create your password. Please try again."
       );
       return selectCriteria();
+    } else {
+      // display selected criteria to user and ask to confirm
+      window.alert(
+        "Your password is ready to go. Please review the criteria you selected. If everything looks good, click on the GENERATE PASSWORD button to create your password. Otherwise, refresh the page to start over. \n\nNumber of Characters: " +
+          passwordObj.char +
+          "\nLowercase Characters Included? " +
+          passwordObj.lower +
+          "\nUppercase Characters Included? " +
+          passwordObj.upper +
+          "\nNumeric Characters Included? " +
+          passwordObj.numeric +
+          "\nSpecial Characters Included? " +
+          passwordObj.special
+      );
     }
-    window.alert("Your password is ready to go. Click 'Generate Password' to proceed.");
   } else {
     char = window.alert("You did not enter a valid number. Please try again.");
     return selectCriteria();
   }
-}
+};
 
-// Write password to the #password input
-var writePassword = function(Obj) {
-  console.log("generate password button clicked");
-  var password = generatePassword(Obj);
+// write password to the #password input
+function writePassword() {
+  var password = generatePassword(passwordObj);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
+// generate random password using password selection criteria provided by user
 function generatePassword(Obj) {
   var passwordArray = [];
   var password = "";
@@ -119,50 +120,54 @@ function generatePassword(Obj) {
     "/",
   ];
 
+  // loop through each password character
   for (var i = 0; i < Obj.char; i++) {
     var charType = Math.floor(Math.random() * (4 - 1 + 1) + 1);
-    console.log("char type: " + charType);
 
+    // random charType is lowercase and user selected lowercase
     if (charType == 1 && Obj.lower == true) {
       charRandom = Math.floor(Math.random() * (122 - 97 + 1) + 97);
-      console.log("random char: " + charRandom);
       var passwordChar = String.fromCharCode(charRandom);
       passwordArray.push(passwordChar);
-      console.log("lower: " + passwordChar);
+
+      // random charType is uppercase and user selected uppercase
     } else if (charType == 2 && Obj.upper == true) {
       charRandom = Math.floor(Math.random() * (90 - 65 + 1) + 65);
-      console.log("random char: " + charRandom);
       var passwordChar = String.fromCharCode(charRandom);
       passwordArray.push(passwordChar);
-      console.log("lower: " + passwordChar);
+
+      // random charType is number and user selected number
     } else if (charType == 3 && Obj.numeric == true) {
       charRandom = Math.floor(Math.random() * (57 - 48 + 1) + 48);
-      console.log("random char: " + charRandom);
       var passwordChar = String.fromCharCode(charRandom);
       passwordArray.push(passwordChar);
-      console.log("lower: " + passwordChar);
+
+      // random charType is special character and user selected special character
     } else if (charType == 4 && Obj.special == true) {
       charRandom = Math.floor(
         Math.random() * (specialCharArray.length - 1 - 0 + 1) + 0
       );
-      console.log("random char: " + charRandom);
       var passwordChar = specialCharArray[charRandom];
       passwordArray.push(passwordChar);
-      console.log("lower: " + passwordChar);
+
+      // re-loop if criteria not met
     } else {
       i -= 1;
     }
   }
-    password = passwordArray.join('');
-    console.log("random password: " + password);
-    return (password);
+
+  // convert passwordArray to string
+  password = passwordArray.join("");
+  return password;
 }
 
-// generate password based on criteria
-window.alert("Welcome to Password Generator. Click on the GENERATE PASSWORD to get started.");
+// welcome message
+window.alert(
+  "Welcome to Password Generator. Please answer the following prompts to get started."
+);
 
-// display password on page
+// prompt user to select password criteria
+selectCriteria();
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", selectCriteria());
-//generateBtn.addEventListener("click", writePassword(passwordObj));
+// add event listener to generate button & run selectCriteria function
+generateBtn.addEventListener("click", writePassword);
